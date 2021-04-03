c87098672f72131e6ce6be9d99e5ab3622bec8fa
Crowdbotics <team@crowdbotics.com>
2021-04-03 19:36:49 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/honu_glass_25446/MainActivity.java
@@ -0,0 +1,15 @@
+package com.honu_glass_25446;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "honu_glass_25446";
+    }
+}
